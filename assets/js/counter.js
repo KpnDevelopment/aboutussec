52acc3f2ff771958e5189eb30b3544fce1492992
kpndev <kpndev@localhost.localdomain>
2020-12-09 03:40:05 +0530

    V1.0

---
+++ assets/js/counter.js
@@ -0,0 +1,25 @@
+// $('[data-toggle="counter-up"]').counterUp({
+//     delay: 10,
+//     time: 1000
+//   });
+
+//   // Testimonials carousel (uses the Owl Carousel library)
+//   $(".testimonials-carousel").owlCarousel({
+//     autoplay: true,
+//     dots: true,
+//     loop: true,
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       768: {
+//         items: 1
+//       },
+//       900: {
+//         items: 2
+//       },
+//       1400: {
+//         items: 3
+//       }
+//     }
+//   });
